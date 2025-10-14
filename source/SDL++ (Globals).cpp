73a396fdffb6151b41037c9b445f9220183d1858
Qabir Qabir <qabirrr@gmail.com>
2025-10-14 20:56:06 +0800

    Split up globals

---
+++ source/SDL++ (Globals).cpp
@@ -0,0 +1,21 @@
+#include "../../include/SDL++ (Globals)"
+
+namespace sdl 
+{
+    SDL_Renderer* g_renderer;
+	bool g_textures_loaded = true;
+	bool g_running = true;
+
+	bool g_left_clicked;
+	bool g_right_clicked;
+	SDL_Point g_mousepos;
+
+	SDL_Window* g_window;
+	int g_width;
+	int g_height;
+	Mix_Music* g_current_music;
+	const uint8_t* g_current_keystate;
+	uint8_t g_previous_keystate[SDL_NUM_SCANCODES] = {0};
+	std::string g_character = "";
+	bool g_char_typed = false;
+}
--- source/SDL++.cpp
+++ source/SDL++.cpp
@@ -1,27 +1,11 @@
 #include "../../include/SDL++"
+#include "../../include/SDL++ (Globals)"
 #include <iostream>
 #include <cstring>
 #include <optional>
 
 namespace sdl
 {
-	SDL_Renderer* g_renderer;
-	bool g_textures_loaded = true;
-	bool g_running = true;
-
-	bool g_left_clicked;
-	bool g_right_clicked;
-	SDL_Point g_mousepos;
-
-	SDL_Window* g_window;
-	int g_width;
-	int g_height;
-	Mix_Music* g_current_music;
-	const uint8_t* g_current_keystate;
-	uint8_t g_previous_keystate[SDL_NUM_SCANCODES] = {0};
-	std::string g_character = "";
-	bool g_char_typed = false;
-
 	bool color_t::operator==(const color_t& other) const
 	{
 		return
